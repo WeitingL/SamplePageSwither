c9455043527a1773463b573807963d80359a4e4b
WeitingL <ss890900@gmail.com>
2022-01-08 17:37:32 +0800

    Create the PageChangeCallback with ViewPager2 and change the tool bar title text and button text.

--- app/src/main/java/com/weiting/samplepageswitcher/MainActivity.kt
+++ app/src/main/java/com/weiting/samplepageswitcher/MainActivity.kt
@@ -3,6 +3,7 @@ package com.weiting.samplepageswitcher
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.weiting.samplepageswitcher.databinding.ActivityMainBinding
 
@@ -16,30 +17,37 @@ class MainActivity : AppCompatActivity() {
         val tabLayout = binding.tlTabs
         val viewPage = binding.vpViewPage
 
-        viewPage.adapter = PageAdapter(this)
+        viewPage.adapter = PageAdapter(this, binding.tvTitle)
+        viewPage.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.apply {
+                    when (position) {
+                        0 -> {
+                            tvTitle.text = "線上測驗A"
+                            btSwitch.text = "切至分頁2"
+                        }
+                        1 -> {
+                            tvTitle.text = "線上測驗B"
+                            btSwitch.text = "切至分頁1"
+                        }
+                    }
+                }
+            }
+        })
 
         binding.btSwitch.setOnClickListener {
-            when(viewPage.getCurrentItem()){
-                0 -> {
-                    viewPage.setCurrentItem(1)
-                }
-                1 -> {
-                    viewPage.setCurrentItem(0)
-                }
-                else -> {
-                    viewPage.setCurrentItem(0)
-                }
+            when (viewPage.getCurrentItem()) {
+                0 -> viewPage.setCurrentItem(1)
+                1 -> viewPage.setCurrentItem(0)
+                else -> viewPage.setCurrentItem(0)
             }
         }
 
-        TabLayoutMediator(tabLayout, viewPage){ tab, position ->
-            when (position){
-                0 -> {
-                    tab.text = "分頁1"
-                }
-                1 -> {
-                    tab.text = "分頁2"
-                }
+        TabLayoutMediator(tabLayout, viewPage) { tab, position ->
+            when (position) {
+                0 -> tab.text = "分頁1"
+                1 -> tab.text = "分頁2"
             }
         }.attach()
 
--- app/src/main/java/com/weiting/samplepageswitcher/PageAdapter.kt
+++ app/src/main/java/com/weiting/samplepageswitcher/PageAdapter.kt
@@ -1,19 +1,29 @@
 package com.weiting.samplepageswitcher
 
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.weiting.samplepageswitcher.ui.FragmentA
 import com.weiting.samplepageswitcher.ui.FragmentB
 
-class PageAdapter(activity: MainActivity) : FragmentStateAdapter(activity) {
+class PageAdapter(activity: MainActivity, val titleView: TextView) : FragmentStateAdapter(activity) {
 
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> FragmentA()
-            1 -> FragmentB()
+            0 -> getFragmentA()
+            1 -> getFragmentB()
             else -> throw Exception("Unknown position $position")
         }
     }
+
+    private fun getFragmentA():FragmentA{
+        return FragmentA()
+    }
+
+    private fun getFragmentB():FragmentB{
+        return FragmentB()
+    }
+
 }
